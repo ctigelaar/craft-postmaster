373b5e51ff6655b34ff6c5c398f7fbe70e83bab4
Christiaan Tigelaar <christiaan@sitemind.nl>
2016-11-15 17:06:15 +0100

    Allow /marshal for cronjob

--- controllers/Postmaster_QueueController.php
+++ controllers/Postmaster_QueueController.php
@@ -3,6 +3,8 @@ namespace Craft;
 
 class Postmaster_QueueController extends BaseController
 {
+	protected $allowAnonymous = true;
+
 	public function actionMarshal()
 	{
 		craft()->postmaster_queue->marshal();
